03430e0fda688bcc819b44db101e9e548dcf906e
MeAusten <35338649+MeAusten@users.noreply.github.com>
2020-04-02 18:31:54 +0600

    Triangle's Area Calculation
    Task1

---
+++ area.rb
@@ -0,0 +1,8 @@
+puts 'What is the base of triangle?'
+base = gets.chomp.to_f
+
+puts 'What is the height of triangle?'
+height = gets.chomp.to_f
+
+puts 'The Area of your triangle is:'   
+puts index = 0.5*(base*height)
